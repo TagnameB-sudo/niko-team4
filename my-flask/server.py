9f2f001b265c4d8713edae49b424a14ad2ad4376
TagnameB-sudo <gyun0515@gmail.com>
2020-08-20 18:25:45 -0700

    server.py and schema.py

---
+++ my-flask/server.py
@@ -0,0 +1,22 @@
+from flask improt Flask
+from flask_graphal import GraphQLView
+from schema import schema
+
+app = Flask(__name__)
+
+@app.route('/')
+def default_route():
+    return "Hello, Default Route!"
+
+@app.route('/index')
+def index():
+    return "Hello,World!"
+@app.route('/get_name')
+def get_name():
+    return "My name flask!"
+
+app.add_url_rule(
+        '/gql',
+        view_func=GraphQLView.ad_view('graphql', schema=schema, graphiql=True)
+        )
+app.run()
